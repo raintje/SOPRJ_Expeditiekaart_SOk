77c19b2d84df4461a4df6e0fff8d727d09e81dba
jasontonk <jjtonk@student.avans.nl>
2021-03-03 23:20:58 +0100

    Migrations and LayerItem factory filled

---
+++ database/migrations/2021_03_03_152806_create_layer_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLayerItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('layer_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('body_preview');
+            $table->text('body');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('layer_items');
+    }
+}
---
+++ database/migrations/2021_03_03_153016_create_first_layer_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFirstLayerItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('first_layer_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('layer_item_id');
+            $table->integer('x_pos');
+            $table->integer('y_pos');
+
+            $table->foreign('layer_item_id')->references('id')->on('layer_items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('first_layer_items');
+    }
+}
