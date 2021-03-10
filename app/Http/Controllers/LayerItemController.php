f79b931c80b2cc3ca194de12a2cb927b2ac27d73
jasontonk <jjtonk@student.avans.nl>
2021-03-10 14:49:51 +0100

    Created empty methods and added routes

--- app/Http/Controllers/LayerItemController.php
+++ app/Http/Controllers/LayerItemController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class LayerItemController extends Controller
 {
-    //
+    public function index(){
+
+
+    }
+
+    public function show($id){
+
+
+    }
+
+    public function create(){
+
+
+    }
+    public function store(){
+
+
+    }
+    public function edit($id){
+
+
+    }
+
+    public function update(){
+
+
+    }
+
+    public function destroy(){
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LayerItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/items', [LayerItemController::class, 'index']);
+Route::get('/items/{id}', [LayerItemController::class, 'show']);
+Route::get('/items/create', [LayerItemController::class, 'create']);
+Route::get('/items/edit/{id}', [LayerItemController::class, 'edit']);
