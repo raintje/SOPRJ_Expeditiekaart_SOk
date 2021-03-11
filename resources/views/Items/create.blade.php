4d33a71c012019123bac2d53acd4f7e85e3da9da
Jacky <j.zheng657@hotmail.com>
2021-03-11 12:48:55 +0100

    Some1

--- resources/views/Items/create.blade.php
+++ resources/views/Items/create.blade.php
@@ -5,14 +5,14 @@
     <form>
         <div class="form-group">
             <label for="titelInput">Titel</label>
-            <input type="text" class="form-control" id="TitelInput" placeholder="Title">
+            <input type="text" class="form-control" name="name" id="TitelInput" placeholder="Title">
         </div>
         <div class="form-group">
             <label> Categorie </label>
             <div class="row">
                 @foreach($categories as $category)
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="category-{{ $category }}">
+                    <input class="form-check-input" type="checkbox" name="category-{{ $category }}" id="category-{{ $category }}">
                     <label class="form-check-label" for="category-{{ $category }}">
                         {{ $category }}
                     </label>
@@ -24,10 +24,27 @@
             <label for="titelInput">Vervolg pad</label>
             <select class="custom-select">
                 <option selected>Opties</option>
-                @foreach($categories as $category)
-                    <option value="2">Two</option>
+                @foreach($existingItems as $item)
+                    <option value="{{ $item->id }}">{{$item->name}}</option>
                 @endforeach
             </select>
+        </div>
+        <div class="form-group">
+            <label for="inhoudInput">Inhoudelijke informatie</label>
+            <div>
+                <div> 
+                    <select class="form-control">
+                        <option>Tekst</option>
+                        <option>Titel</option>
+                    </select>
+                    <button><strong>B</strong></button>
+                    <button><em>I</em></button>
+                    <button><u>U</u></button>
+                    <button><img src=""></button>
+                    <button><img src=""></button>
+                </div> 
+                <textarea class="form-control" id="inhoudInput" rows="15"> </textarea>  
+            </div>
         </div>
     </form>
 </div>
