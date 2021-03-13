92320501776afba5b17de4b3dc6f353d81b1b45d
Jacky <j.zheng657@hotmail.com>
2021-03-13 01:59:48 +0100

    item form

---
+++ public/js/item-form.js
@@ -0,0 +1,56 @@
+function AddItemPath(elem, source){
+    if(elem.selectedIndex != 0){
+        let sourceElem = document.getElementById(source);
+        let tagElem = createTagElement(elem);
+
+        sourceElem.appendChild(tagElem);
+
+        RemoveItemFromSelect(elem);
+    }
+}
+
+function RemoveItemPath(tagElem, selectElem, text, value){
+    let optionElem = document.createElement("OPTION");
+    optionElem.text = text;
+    optionElem.setAttribute("value", value);
+    selectElem.add(optionElem);
+
+    tagElem.parentNode.removeChild(tagElem);
+
+}
+
+function RemoveItemFromSelect(selectElement){
+    selectElement.remove(selectElement.selectedIndex);
+}
+
+function createTagElement(elem){
+    let text = elem.options[elem.selectedIndex].text;
+
+    //tag (Main body) 
+    let tagElem = document.createElement("DIV");
+    tagElem.classList.add("tag");
+    
+    //close button
+    let closeElem = document.createElement("DIV");
+    closeElem.classList.add("tag-close");
+    closeElem.innerHTML = "x";
+    closeElem.onclick = function() { RemoveItemPath(tagElem, elem, text, elem.value) };
+
+    //text
+    let textElem = document.createElement("DIV");
+    textElem.classList.add("tag-text");
+    textElem.innerHTML = text;
+
+    //hidden input
+    let inputElem = document.createElement("INPUT");
+    inputElem.setAttribute("type", "hidden");
+    inputElem.setAttribute("name", "itemLinks[]");
+    inputElem.setAttribute("value", elem.value);
+
+    //append
+    tagElem.appendChild(closeElem);
+    tagElem.appendChild(textElem);
+    tagElem.appendChild(inputElem);
+
+    return tagElem;
+}
--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html> <!--standard mode for Tiny plugin -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://cdn.tiny.cloud/1/2t1jg49md5wferhnxq0lnsjm72c9ghml73cho300vr1sgv9w/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="/js/item-form.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/css/item-form.css"/>
 <script>
     tinymce.init({
       selector: '#inhoudInput'
     });
 </script>
-<div class="container">
+<div class="container">.
     <h1>Items aanmaken</h1>
 
     <form method="POST" action="/items">
@@ -25,17 +29,25 @@
         </div>
         <div class="form-group">
             <label for="titelInput">Vervolg pad</label>
-            <select class="custom-select">
+            <select onchange="AddItemPath(this, 'item-links-container')" class="custom-select">
                 <option selected>Opties</option>
                 @foreach($existingItems as $item)
                     <option value="{{ $item->id }}">{{$item->title}}</option>
                 @endforeach
             </select>
+            <div id="item-links-container"> 
+
+            </div>
         </div>
         <div class="form-group">
             <label for="inhoudInput">Inhoud</label>
             <textarea class="form-control" id="inhoudInput" name="inhoudInput" rows="15"> </textarea>
         </div>
+
+        <div class="custom-file">
+            <input id="input-id" type="file" class="file" data-preview-file-type="text" >
+        </div>
+
         <button type="submit" class="btn btn-primary"> Opslaan </button>
     </form>
 </div>
