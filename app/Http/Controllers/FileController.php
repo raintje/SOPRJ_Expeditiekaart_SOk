3a67780c0e59432935ed7bbbad96929c67e34f66
Raintje <wesselvandorsten@gmail.com>
2021-03-14 10:13:53 +0100

    start validation

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class FileController extends Controller
 {
     public function store(Request $request) {
-
+        $validated = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'path' => 'required',
+        ]);
     }
 }
