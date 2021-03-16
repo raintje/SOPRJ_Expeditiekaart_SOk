25c32676c9aa082f5a91018562f671e7075fcbd3
Jacky <j.zheng657@hotmail.com>
2021-03-16 15:38:50 +0100

    Updated

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -2,28 +2,16 @@
 <!--standard mode for Tiny plugin -->
 <script src="https://cdn.tiny.cloud/1/2t1jg49md5wferhnxq0lnsjm72c9ghml73cho300vr1sgv9w/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 <!-- external lib -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-<!-- the main fileinput plugin file -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" crossorigin="anonymous">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/js/plugins/piexif.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/js/plugins/sortable.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/js/fileinput.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/themes/fas/theme.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.9/js/locales/nl.js"></script>
-
+<!-- custom -->
 <link rel="stylesheet" href="/css/item-form.css"/>
 <script src="/js/item-form.js"></script>
 
 <div class="container">
     <h1>Items aanmaken</h1>
 
-    <form method="POST" action="/items">
+    <form method="POST" action="/items" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
             <label for="titelInput">Titel</label>
@@ -31,7 +19,7 @@
         </div>
         <div class="form-group">
             <label> Categorie </label>
-            <div class="item-row">
+            <div class="d-flex">
                 @foreach($categories as $category)
                 <div class="form-check custom-category-check">
                     <input class="form-check-input" type="checkbox" name="categories[]" value="{{ $category }}" id="category-{{ $category }}">
@@ -60,7 +48,7 @@
         </div>
 
         <div class="form-group">
-            <input id="input-krajee" type="file" name="files" class="file" data-preview-file-type="text" >
+            <input type="file" name="files[]" class="file" multiple>
         </div>
 
         <button type="submit" class="btn btn-primary"> Opslaan </button>
@@ -71,17 +59,5 @@
     tinymce.init({
       selector: '#inhoudInput',
       language: 'nl'
-    });
-
-    $("#input-krajee").fileinput({
-        theme: "fas",
-        uploadUrl: "/items",
-        language: "nl"
-    });
-
-    $('option').mousedown(function(e) {
-        e.preventDefault();
-        $(this).prop('selected', !$(this).prop('selected'));
-        return false;
     });
 </script>
