cea3757db943bd167eb672c77958e9fe253d947d
Vincent <63718020+Vincent2162102@users.noreply.github.com>
2021-03-18 20:51:44 +0100

    fixed one test

--- tests/Browser/CreateItemTest.php
+++ tests/Browser/CreateItemTest.php
@@ -49,8 +49,8 @@ class CreateItemTest extends DuskTestCase
             $browser->visit('/items/create')
                 ->type('title', 'Voluptatem atque amet autem dicta blanditiis.')
                 ->press('Opslaan')
-                ->pause(5000)
                 ->assertSee('De titel moet uniek zijn.')
+                ->assertSee('De inhoud van het item mag niet leeg zijn.')
                 ->assertPathIs('/items/create');
         });
     }
