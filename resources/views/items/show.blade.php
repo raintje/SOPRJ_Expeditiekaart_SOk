525a917df4fd2f63067215e1ae5c26d255f27407
jasontonk <jjtonk@student.avans.nl>
2021-03-24 17:16:57 +0100

    changed styling and made linkeditems working

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -18,11 +18,12 @@
     @if($files != null) <!-- TODO check if files is empty not null -->
         <div class="m-2">
             <h3>Bijlages:</h3>
-            <div class="row justify-content-start">
+            <div class="m-2 row justify-content-start">
                 @foreach($files as $file)
                     <div class="card align-items-center text-center m-2 col-md-3">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title">{{$file->title}}</h5>
+                            <a href="#" class="btn btn-primary w-100 mt-auto">Download</a>
                         </div>
                     </div>
                 @endforeach
@@ -33,10 +34,10 @@
     @if($linkedItems != null) <!-- TODO check if linkeditems is empty not null -->
         <div class="m-2">
             <h3>Vervolg paden:</h3>
-            <div class="row justify-content-start">
+            <div class="m-2 row justify-content-start">
                 @foreach($linkedItems as $linkedItem)
-                    <div class="border-left border-info align-items-center text-center m-2 col-md-3">
-                        <a class="text-info" href="#">{{$linkedItem->title}}</a>
+                    <div class=" align-items-center text-center mb-2 mt-2 col-md-3">
+                        <a class="btn btn-outline-info w-100 h-100" href="{{route('show.item', [$linkedItem->id])}}">{{$linkedItem->title}}</a>
                     </div>
                 @endforeach
             </div>
