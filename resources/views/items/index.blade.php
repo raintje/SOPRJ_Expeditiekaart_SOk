f1ef417401ae7d8a1ba754d0bb1e5c7966951a81
Raintje <wesselvandorsten@gmail.com>
2021-03-25 13:38:18 +0100

    button naar edit toegevoegd aan item overzicht

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,4 +1,4 @@
 @foreach($items as $item)
-    <p>This is item {{$item->title}}</p>
+    <p>This is item {{$item->title}} - <a href="{{ url('/items/' . $item->id . '/edit') }}" class="btn btn-primary mt-auto">Aanpassen</a></p>
 @endforeach
 
