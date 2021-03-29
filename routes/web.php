85f98c787ba7e7478bb57289b0a18897e44be43e
Tommy den Reijer <t.ommy@hotmail.nl>
2021-03-29 11:55:58 +0200

    ttc

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,9 @@ Route::post('/items/edit/location/save', [FirstLayerItemController::class, 'save
 Route::get('/items/{id}', [LayerItemController::class, 'show'])->name('show.item');
 Route::get('/items/{id}/edit', [LayerItemController::class, 'edit'])->name('edit.item');
 Route::put('/items/{id}', [LayerItemController::class, 'update'])->name('update.item');
+
+Auth::routes();
+
+
+
+Route::get('/files/{id}', [LayerItemController::class, 'downloadFile'])->name('download.file');
