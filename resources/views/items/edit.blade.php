445f4862647df5fabd3c5da3482471f4b1336e35
Raintje <wesselvandorsten@gmail.com>
2021-04-02 09:44:19 +0200

    formatting

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -1,42 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h4 class="text-center">Item {{ $item->title }} aanpassen </h1>
-        @if(count($errors) > 0 )
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <ul class="p-0 m-0" style="list-style: none;">
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+    <div class="container">
+        <h4 class="text-center">Item {{ $item->title }} aanpassen </h1>
+            @if (count($errors) > 0)
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <ul class="p-0 m-0" style="list-style: none;">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <form method="PUT" action="{{ route('update.item', array('id' => $item->id)) }}" enctype="multipart/form-data">
-            @csrf
-            {{-- Title --}}
-            <div class="form-group">
-                <label for="titelInput">Titel</label>
-                <input type="text" class="form-control" name="title" id="TitelInput" placeholder="Titel" value="{{ $item->title }}">
-            </div>
+            <form method="PUT" action="{{ route('update.item', ['id' => $item->id]) }}"
+                enctype="multipart/form-data">
+                @csrf
+                {{-- Title --}}
+                <div class="form-group">
+                    <label for="titelInput">Titel</label>
+                    <input type="text" class="form-control" name="title" id="TitelInput" placeholder="Titel"
+                        value="{{ $item->title }}">
+                </div>
 
-            {{-- Categories --}}
-            <div class="form-group">
-                <label> Categorie </label>
-                <div class="d-flex flex-column flex-sm-row">
-                    @foreach($categories as $category)
-                    <div class="form-check custom-category-check mb-1">
-                        <input class="form-check-input" type="checkbox" name="categories[]" value="{{ $category->id }}" id="category-{{ $category->id }}" @if(!empty($itemcategories)) @if($itemcategories->contains($category))
-                        checked>
-                        @else
-                        >
-                        @endif
-                        @else
-                        >
+                {{-- Categories --}}
+                <div class="form-group">
+                    <label> Categorie </label>
+                    <div class="d-flex flex-column flex-sm-row">
+                        @foreach ($categories as $category)
+                            <div class="form-check custom-category-check mb-1">
+                                <input class="form-check-input" type="checkbox" name="categories[]"
+                                    value="{{ $category->id }}" id="category-{{ $category->id }}" @if (!empty($itemcategories))  @if ($itemcategories->contains($category))
+                                checked>
+                            @else
+                                > @endif
+                            @else
+                                >
                         @endif
                         <label class="form-check-label" for="category-{{ $category->id }}">
                             {{ $category->name }}
@@ -44,79 +46,79 @@
                     </div>
                     @endforeach
                 </div>
-            </div>
+    </div>
 
-            {{-- Selectlist linked items --}}
-            <div class="form-group">
-                <label for="itemPathSelect">Vervolgpaden</label>
-                @if(!empty($linkedItems))
-                <select id="itemPathSelect" class="custom-select mb-2 sm:flex sm:flex-col">
-                    <option selected>Opties</option>
-                    @foreach($existingItems as $item)
-                    @if(!$linkedItems->contains($item))
-                    <option value="{{ $item->id }}">{{$item->title}}</option>
+    {{-- Selectlist linked items --}}
+    <div class="form-group">
+        <label for="itemPathSelect">Vervolgpaden</label>
+        @if (!empty($linkedItems))
+            <select id="itemPathSelect" class="custom-select mb-2 sm:flex sm:flex-col">
+                <option selected>Opties</option>
+                @foreach ($existingItems as $item)
+                    @if (!$linkedItems->contains($item))
+                        <option value="{{ $item->id }}">{{ $item->title }}</option>
                     @endif
-                    @endforeach
-                </select>
-                @endif
-                <div id="item-links-container">
-                    {{-- TODO: Show existing links --}}
-                </div>
-            </div>
+                @endforeach
+            </select>
+        @endif
+        <div id="item-links-container">
+            {{-- TODO: Show existing links --}}
+        </div>
+    </div>
 
-            {{-- Body --}}
-            <div class="form-group">
-                <label for="inhoudInput">Inhoud</label>
-                <textarea class="form-control" id="inhoudInput" name="body" rows="15">{{ $item->body }}</textarea>
-            </div>
+    {{-- Body --}}
+    <div class="form-group">
+        <label for="inhoudInput">Inhoud</label>
+        <textarea class="form-control" id="inhoudInput" name="body" rows="15">{{ $item->body }}</textarea>
+    </div>
 
-            {{-- Linked files --}}
-            @if(!empty($files))
-            <div class="m-2">
-                <h3>Bijlages:</h3>
-                <div class="m-2 row justify-content-start">
-                    @foreach($files as $file)
+    {{-- Linked files --}}
+    @if (!empty($files))
+        <div class="m-2">
+            <h3>Bijlages:</h3>
+            <div class="m-2 row justify-content-start">
+                @foreach ($files as $file)
                     <div class="card align-items-center text-center m-2 col-md-3">
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">{{ $file->title }}</h5>
                             <a href="#" class="btn btn-primary w-100 mt-auto">Verwijderen</a>
                         </div>
                     </div>
-                    @endforeach
-                </div>
+                @endforeach
             </div>
-            @endif
+        </div>
+    @endif
 
-            {{-- Upload files --}}
-            <div class="form-group">
-                <input type="file" name="files[]" class="file outline--none" multiple>
-            </div>
+    {{-- Upload files --}}
+    <div class="form-group">
+        <input type="file" name="files[]" class="file outline--none" multiple>
+    </div>
 
-            {{-- Form submit --}}
-            <button type="submit" class="btn btn-primary mb-1"> Wijzigingen opslaan </button>
+    {{-- Form submit --}}
+    <button type="submit" class="btn btn-primary mb-1"> Wijzigingen opslaan </button>
 
-        </form>
-</div>
+    </form>
+    </div>
 @endsection
 
 @section('script')
-<script>
-    tinymce.init({
-        selector: '#inhoudInput'
-        , language: 'nl'
-    });
+    <script>
+        tinymce.init({
+            selector: '#inhoudInput',
+            language: 'nl'
+        });
 
-    function ValidateSize(file) {
-        let FileSize = file.files[0].size / 1024 / 1024; // in MB
-        if (FileSize > 2) {
-            alert('Bestand mag niet groter zijn dan 2MB');
-            $(file).val(''); //for clearing with Jquery
-        } else {
+        function ValidateSize(file) {
+            let FileSize = file.files[0].size / 1024 / 1024; // in MB
+            if (FileSize > 2) {
+                alert('Bestand mag niet groter zijn dan 2MB');
+                $(file).val(''); //for clearing with Jquery
+            } else {
 
+            }
         }
-    }
 
-</script>
-<script src="{{ asset('js/item-form.js') }}" defer></script>
-<script src="{{ mix('js/app.js') }}"></script>
+    </script>
+    <script src="{{ asset('js/item-form.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 @endsection
