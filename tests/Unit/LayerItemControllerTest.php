85d0b4aaae1ba56af50c2a0ab78ddab991f53127
Jason Tonk <63650211+jasontonk@users.noreply.github.com>
2021-04-12 15:34:30 +0200

    Feature/unit test delete (#51)
    * delete unwanted test
    
    * added unit test for destroy method
    
    * changed config to work with all databases
    
    * changed tests to use route names
    
    Co-authored-by: jasontonk <jjtonk@student.avans.nl>

--- tests/Unit/LayerItemControllerTest.php
+++ tests/Unit/LayerItemControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\Category;
+use App\Models\File;
 use App\Models\LayerItem;
 use App\Models\FirstLayerItem;
 use Tests\TestCase;
@@ -13,7 +14,7 @@ class LayerItemControllerTest extends TestCase
 
     use WithFaker;
 
-    protected function setUpFaker() 
+    protected function setUpFaker()
     {
         $this->faker = $this->makeFaker('nl_NL');
     }
@@ -33,7 +34,7 @@ class LayerItemControllerTest extends TestCase
      * Tests if all the information from the database is correctly parsed to a list.
      * @return void
      */
-    public function test_categories_are_present_in_database() 
+    public function test_categories_are_present_in_database()
     {
         $haystack = Category::all();
         $id = rand(1, 3);
@@ -48,7 +49,7 @@ class LayerItemControllerTest extends TestCase
     {
         $testbody = $this->faker('nl_NL')->text;
         $mock = new LayerItem();
-        $mock->body = $testbody;  
+        $mock->body = $testbody;
         $this->assertTrue($mock->body == $testbody);
     }
 
@@ -56,12 +57,64 @@ class LayerItemControllerTest extends TestCase
      * Tests if the given ID gets attached to the model.
      * @return void
      */
-    public function test_category_id_gets_attached_to_model() 
+    public function test_category_id_gets_attached_to_model()
     {
         $category = $this->faker->randomElement(Category::all()->pluck('id')->toArray());
         $mock = new FirstLayerItem();
         $mock->layer_item_id = $category;
         $this->assertTrue($mock->layer_item_id == $category);
     }
+
+    /**
+     * Tests if not existing item gives 404.
+     * @return void
+     */
+    public function test_not_existing_item_gives_404_on_destroy()
+    {
+        $layerItem = LayerItem::all()->last();
+        $response = $this->get(route('destroy.item', ($layerItem->id+1)));
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Tests if existing item can be destroyed.
+     * @return void
+     */
+    public function test_existing_item_can_be_destroyed()
+    {
+        $layerItem = LayerItem::factory()->create();
+        $this->get(route('destroy.item', $layerItem->id));
+        $layerItem = LayerItem::find($layerItem->id);
+        $this->assertTrue($layerItem == null);
+    }
+
+    /**
+     * Tests if existing item with firstLayerItem can be destroyed.
+     * @return void
+     */
+    public function test_existing_item_with_firstLayerItem_can_be_destroyed()
+    {
+        $layerItem = LayerItem::factory()->create();
+        $firstLayerItem = FirstLayerItem::factory()->create(['layer_item_id' => $layerItem->id]);
+        $this->get(route('destroy.item', $layerItem->id));
+        $layerItem = LayerItem::find($layerItem->id);
+        $firstLayerItem = FirstLayerItem::find($firstLayerItem->id);
+        $this->assertTrue($layerItem == null && $firstLayerItem == null);
+    }
+
+    /**
+     * Tests if existing item with a file can be destroyed.
+     * @return void
+     */
+    public function test_existing_item_with_file_can_be_destroyed()
+    {
+        $layerItem = LayerItem::factory()->create();
+        $file = File::factory()->create(['layer_item_id' => $layerItem->id]);
+        $this->get(route('destroy.item', $layerItem->id));
+        $layerItem = LayerItem::find($layerItem->id);
+        $file = File::find($file->id);
+        $this->assertTrue($layerItem == null && $file == null);
+    }
+
 
 }
