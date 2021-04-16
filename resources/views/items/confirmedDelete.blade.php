65ac8b0708fcb33713e00fec4ef17b9e22b02aaa
Raintje <wesselvandorsten@gmail.com>
2021-04-16 11:53:09 +0200

    Revert "merge conflict fix?"
    This reverts commit 18eda2e32574133f7d09daec7c0354a015bc8cf0, reversing
    changes made to e539be15521d6ea2145707efc495cbc028f7cf5a.

--- resources/views/items/confirmedDelete.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h3>Het item is succesvol verwijderd!</h3>
-        <a href="{{route('home')}}" class="btn btn-primary">Terug naar de expeditiekaart</a>
-    </div>
-@endsection
