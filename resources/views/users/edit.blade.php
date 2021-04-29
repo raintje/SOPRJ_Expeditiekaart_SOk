490f07d48fd376a3ed350d7f010236aac2e2c7aa
Vincent <63718020+Vincent2162102@users.noreply.github.com>
2021-04-29 18:35:16 +0100

    added change password

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,12 +6,12 @@
 
     <div class="container">
         <div class="row gutters">
-            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+            <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
                 <div class="card h-100">
                     <div class="card-body">
                         <div class="row gutters mb-4">
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <h6 class="mb-2 text-primary">Personal Details</h6>
+                                <h6 class="mb-3"><b>Personal Details</b></h6>
                             </div>
                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                 <div class="form-group">
@@ -39,10 +39,27 @@
             </div>
 
 
-            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
                 <div class="card h-100">
                     <div class="card-body">
 
+                            <div class="col-12 col-sm-12 mb-3">
+                                <div class="mb-3"><b>Verander Wachtwoord</b></div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label>New Password</label>
+                                            <input class="form-control" type="password" placeholder="••••••">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
+                                            <input class="form-control" type="password" placeholder="••••••"></div>
+                                    </div>
+                                </div>
                         <div class="modal-footer">
                             <button type="button" id="submit" name="submit" class="btn btn-secondary">Cancel</button>
                             <button type="button" id="submit" name="submit" class="btn btn-primary">Update</button>
