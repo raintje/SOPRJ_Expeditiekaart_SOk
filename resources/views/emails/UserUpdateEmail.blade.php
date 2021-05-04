7b801ff53b9fec6128c040bfd975dc9b877b2808
jasontonk <jj.tonk@student.avans.nl>
2021-05-04 16:39:36 +0200

    Added email template for user update

---
+++ resources/views/emails/UserUpdateEmail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gebruiker Gewijzigd</title>
+</head>
+    <body>
+        <p>Beste {{ $details['name'] }}, </p>
+        <br>
+        <p> De gegevens van uw account met email: <u>{{ $details['email'] }}</u> zijn gewijzigd voor de Expeditiekaart Voor Bedrijfsovername in de Agrarische sector.</p>
+        <br>
+        <br>
+        <p>Dit is een automatisch gegenereerd bericht en reacties op deze mail worden niet gelezen.</p>
+    </body>
+</html>
