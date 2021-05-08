a6a6669b8c1b594a512dc053a2e35cf5ec2c8384
Raintje <wesselvandorsten@gmail.com>
2021-05-08 13:28:32 +0200

    re-added deleted routes, fixed bad calls in user index and create

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,14 @@ use Illuminate\Support\Facades\Route;
 // HomeController routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::group(['middleware' => ['auth']], function () {
 // UserController routes
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users');
     Route::get('api/users', [UserController::class, 'getUsers'])->name('get.user');
+    Route::get('/users/create', [UserController::class, 'create'])->name('create.user');
     Route::put('/users/update/password/{user}', [UserController::class, 'updatePassword'])->name('user.update.password');
+    Route::post('/users', [UserController::class, 'store'])->name('store.user');
     Route::post('/users/delete', [UserController::class, 'destroy'])->name('destroy.user');
-    Route::resource('users', UserController::class);
 });
 
 // DashboardController routes
