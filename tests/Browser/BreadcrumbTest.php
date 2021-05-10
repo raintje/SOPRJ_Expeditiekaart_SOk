a950e8ab58569a1b18d821fa24fae30e5c42af68
Jacky <j.zheng657@hotmail.com>
2021-05-10 07:37:54 +0200

    Contains breadcrumb link test added

--- tests/Browser/BreadcrumbTest.php
+++ tests/Browser/BreadcrumbTest.php
@@ -2,17 +2,67 @@
 
 namespace Tests\Browser;
 
+use App\Models\LayerItem;
+use Facebook\WebDriver\WebDriverBy;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class BreadcrumbTest extends DuskTestCase
 {
+
+    use WithFaker;
+
+    protected function setUpFaker()
+    {
+        $this->faker = $this->makeFaker('nl_NL');
+    }
+
+    /**
+     * @group test     
+     * @group breadcrumb
+     */
+    public function testContainsLinks()
+    {
+        $this->browse(function (Browser $browser) {
+            $item = $this->faker()->randomElement(LayerItem::all());
+
+            $browser->visitRoute('show.item', ['id' => $item])
+                    ->assertSeeIn('@breadcrumb-list', $item->title);
+
+        });
+    }
+
     /**
      * @group map     
      * @group breadcrumb
      */
-    public function testExample()
+    public function testLinkCanUpdate()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+
+    /**
+     * @group map     
+     * @group breadcrumb
+     */
+    public function testLinkCanFail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+
+    /**
+     * @group map     
+     * @group breadcrumb
+     */
+    public function testLinkCanReturn()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
