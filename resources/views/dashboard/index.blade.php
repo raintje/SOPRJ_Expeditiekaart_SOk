3a055037eb0c6a00036fd64701b9755d496218cd
Tommy den Reijer <t.ommy@hotmail.nl>
2021-05-11 16:28:27 +0200

    fixed nav

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('head_script')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+
 @section('content')
 
     <div class="pb-3">
@@ -9,5 +13,7 @@
     <hr>
     <a href="{{route('users.index')}}"><button class="btn btn-primary">Gebruikers overzicht</button></a>
     <a href="{{route('items')}}"><button class="btn btn-primary">Item overzicht</button></a>
+
+    <script src="{{ mix('js/app.js') }}"></script>
 
 @endsection
