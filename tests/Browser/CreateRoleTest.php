9e6bb4f298df046d1cdc06791d27af96c2296141
wdorsten <wesselvandorsten@gmail.com>
2021-05-24 15:30:35 +0200

    init

---
+++ tests/Browser/CreateRoleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CreateRoleTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::first())
+                    ->visit('/users')
+                    ->press('Rol aanmaken')
+                    ->assertPathIs('/roles/create');
+        });
+    }
+}
