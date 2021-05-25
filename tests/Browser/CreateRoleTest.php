2543c9f1a948f355f461e153aa28764f99d9c291
wdorsten <wesselvandorsten@gmail.com>
2021-05-25 11:20:49 +0200

    wrote tests, fixed validation

--- tests/Browser/CreateRoleTest.php
+++ tests/Browser/CreateRoleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\LayerItem;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
@@ -10,17 +11,55 @@ use Tests\DuskTestCase;
 class CreateRoleTest extends DuskTestCase
 {
     /**
-     * A Dusk test example.
+     * Asserts that the page is navigable.
      *
+     * @group roles.create
      * @return void
      */
-    public function testExample()
+    public function testButtonNavigatesToPage()
     {
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::first())
-                    ->visit('/users')
+                    ->visitRoute('users.index')
                     ->press('Rol aanmaken')
                     ->assertPathIs('/roles/create');
         });
     }
+
+    /**
+     * Asserts that validation errors are displayed upon not entering any data.
+     * 
+     * @group roles.create
+     * @return void
+     */
+    public function testValidationIsDisplayedOnEmpty() 
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::first())
+                    ->visitRoute('roles.create')
+                    ->press('Rol opslaan')
+                    ->assertRouteIs('roles.create')
+                    ->assertSee('De naam van de rol kan niet leeggelaten worden.')
+                    ->assertSee('Er moet minimaal 1 item geselecteerd worden');
+        });
+    }
+
+    /**
+     * Asserts that validation errors are displayed upon entering a name that's too long.
+     * 
+     * @group roles.create
+     * @return void
+     */
+    public function testValidationIsDisplayedOnTooLongName()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::first())
+                    ->visitRoute('roles.create')
+                    ->type('name', str_repeat('a', 200))
+                    ->press('Rol opslaan')
+                    ->assertRouteIs('roles.create')
+                    ->assertSee('Een naam mag maximaal 191 karakters lang zijn');
+        });
+    }
+
 }
