1cb2a258543d2688e71d4a68f1619659e2c91e17
jasontonk <jj.tonk@student.avans.nl>
2021-05-27 15:12:23 +0200

    added routes and filled methods

--- app/Http/Controllers/ItemHistoryController.php
+++ app/Http/Controllers/ItemHistoryController.php
@@ -8,11 +8,23 @@ use Panoscape\History\History;
 
 class ItemHistoryController extends Controller
 {
-    public function revertItem($id)
+    public function restoreItem($id)
     {
         $itemHistoryEdit = History::findOrFail($id);
         $item = LayerItem::findOrFail($itemHistoryEdit->model_id);
+        $changes = $itemHistoryEdit->meta;
 
+        foreach($changes as $change) {
+            $editOn = $change['key'];
+            switch ($editOn) {
+                case 'title':
+                    $item->title = $change['old'];
+                case 'body':
+                    $item->body = $change['old'];
+            }
+        }
+        $item->save();
+        return redirect()->route('show.item', $item->id);
     }
 
 
