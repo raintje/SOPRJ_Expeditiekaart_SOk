1c497af69f0d7eb295fd27ebeed459338254a1f4
Tommy den Reijer <t.ommy@hotmail.nl>
2021-05-30 19:39:42 +0200

    added validation to requests

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -14,7 +14,7 @@ class UserStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        if (Auth::check()){
+        if (Auth::check() && Auth::user()->hasRole('super admin')){
             return true;
         }
     }
