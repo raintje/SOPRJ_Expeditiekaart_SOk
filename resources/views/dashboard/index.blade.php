0744dcedec78705caff6328670939cc3e7aa5951
jasontonk <jj.tonk@student.avans.nl>
2021-06-01 14:56:13 +0200

    Started on newsfeed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,6 +14,27 @@
     <a href="{{route('users.index')}}"><button class="btn btn-primary">Gebruikers overzicht</button></a>
     <a href="{{route('items')}}"><button class="btn btn-primary">Item overzicht</button></a>
 
+    <div class="row mt-5">
+        <div class="col-md p-2 w-100">
+            <p class="font-weight-bold">Nieuws</p>
+            <hr>
+            @foreach($itemHistories as $history)
+                <div>
+                @if($history->message == 'Updating LayerItem ')
+                <p>item {{$history->model_id}} is aangepast door gebruiker {{$history->user_id}}</p>
+                @elseif($history->message == 'Deleting LayerItem ')
+                    <p>item {{$history->model_id}} is verwijderd door gebruiker {{$history->user_id}}</p>
+                @endif
+                </div>
+            @endforeach
+
+        </div>
+        <div class="col-md p-2 w-100">
+            <p class="font-weight-bold">gebruiksgegevens</p>
+            <hr>
+        </div>
+    </div>
+
     <script src="{{ mix('js/app.js') }}"></script>
 
 @endsection
