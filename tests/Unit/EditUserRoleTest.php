8d091b2c37e45bed8b65c6ba5fb07d6897e0a69a
wdorsten <wesselvandorsten@gmail.com>
2021-06-01 14:18:11 +0200

    added function tests

--- tests/Unit/EditUserRoleTest.php
+++ tests/Unit/EditUserRoleTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\UserController;
+use App\Http\Requests\RoleAssignRequest;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
@@ -49,6 +51,8 @@ class EditUserRoleTest extends TestCase
 
     /**
      * Navigates to the relevant page and asserts that it contains the required data.
+     * 
+     * @return void
      */
     public function testEditUserPageContainsRoles() 
     {
@@ -58,4 +62,51 @@ class EditUserRoleTest extends TestCase
         $response = $this->get(route('users.edit', $ranUser->id));
         $response->assertViewHas('roles');
     }
+
+    /**
+     * Tests the controller function for the updating of a user's role with correct data.
+     * 
+     * @return void
+     */
+    public function testEditUserRoleControllerFunction()
+    {
+        // Controller
+        $controller = new UserController();
+
+        // Request for parameter
+        $request = RoleAssignRequest::create('/users/', 'POST', [
+           'role' => $this->faker()->randomElement(Role::all()),
+        ]);
+
+        // Response
+        $user = $this->faker()->randomElement(User::all()->pluck('id'));
+        $response = $controller->updateRole($request, $user);
+
+        // Assert status
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /**
+     * Tests the controller function for the updating of a user's role with incorrect data.
+     * 
+     * @return void
+     */
+    public function testEditUserRoleControllerFunctionWithIncorrectData()
+    {
+        // Define variables
+        $user = User::first();
+        $ranUser = $this->faker()->randomElement(User::all()->except('admin'));
+
+        // Login as admin
+        Auth::login($user);
+
+        // Create request
+        $response = $this->put(route('user.update.role', ['user' => $ranUser->id]), [
+            'role' => null,
+        ]);
+
+        // Assert session has errors.
+        $response->assertSessionHasErrors('role');
+    }
+    
 }
