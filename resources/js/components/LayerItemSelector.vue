80b2299d4afb05c4972e7d63aa2a5b1682a933d0
Vincent <63718020+Vincent2162102@users.noreply.github.com>
2021-06-23 00:00:35 +0100

    added live update, still need to put back deleted values from select list

--- resources/js/components/LayerItemSelector.vue
+++ resources/js/components/LayerItemSelector.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        
+        <div class="form-group">
+            <select name='level' v-model="option" @change="onChange()" class="form-control" id="layerInput">
+                <option v-for="index in 4" :value="index">{{ index }}</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="itemPathSelect">Vervolgpaden<i class="fas fa-info-circle" rel="tooltip" :title="infoPath"></i></label>
+            <select id="itemPathSelect" class="custom-select mb-2 sm:flex sm:flex-col">
+                <option selected>Opties</option>
+                <option v-for="item in this.getSelected" :value="item.id" v-text="item.title"></option>
+            </select>
+        </div>
+        <div id="item-links-container">
+            <div class="tag">
+                <div class="tag-text">Nemo est eius et quia consequuntur et commodi vel.</div>
+                <div class="tag-close">x</div>
+                <input type="hidden" name="itemLinks[]" value="19"></div>
+        </div>
+
     </div>
 </template>
 
@@ -9,13 +28,36 @@ export default {
     props: ['level', 'infoLayer', 'infoPath'],
     data() {
         return {
+            option: "",
             layerItems: []
         }
     },
     mounted() {
-        axios
-            .get('/api/all-layeritems')
-            .then(response => (this.layerItems = response.data))
+        this.getItems();
+        this.option = parseInt(this.level);
     },
+    methods: {
+        onChange() {
+            let e = document.querySelector("#item-links-container");
+
+            let child = e.lastElementChild;
+            while (child) {
+                e.removeChild(child);
+                child = e.lastElementChild;
+            }
+            this.getItems();
+        },
+        getItems() {
+            axios
+                .get('/api/not-firstLayeritems')
+                .then(response => (this.layerItems = response.data)
+                    .then(console.log(response.data)))
+        },
+    },
+    computed: {
+        getSelected: function () {
+            return this.layerItems.filter(e => e.level === parseInt(this.option) + 1)
+        }
+    }
 }
 </script>
